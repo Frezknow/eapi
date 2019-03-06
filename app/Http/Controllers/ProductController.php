a52cb43573562d79ae8716d7404a5ef9c7bc6c1b
Nathan Reynolds <frezknow@gmail.com>
2019-03-06 15:23:09 -0500

    Product collection resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Product;
 use Illuminate\Http\Request;
+use App\Http\Resources\Product\ProductColletion;
 use App\Http\Resources\Product\ProductResource;
 class ProductController extends Controller
 {
@@ -14,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-       return Product::all();
+       return ProductColletion::collection(Product::all());
     }
 
     /**
--- app/Http/Resources/Product/ProductColletion.php
+++ app/Http/Resources/Product/ProductColletion.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\JsonResourceColletion;
-class ProductColletion extends JsonResourceColletion
+class ProductColletion extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,6 +14,14 @@ class ProductColletion extends JsonResourceColletion
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return[
+          'name'=>$this->name,
+          'totalPrice'=> round((1-($this->discount/100))*$this->price,2),
+          'rating'=>$this->reviews->count() >0?round($this->reviews->sum('star')/$this->reviews->count()):'No rating yet.',
+          'discount'=>$this->discount,
+          'href'=>[
+            'link'=>route('products.show',$this->id)
+          ]
+        ];
     }
 }
